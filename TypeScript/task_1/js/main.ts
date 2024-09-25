0c21d9bf9c2cda2c56935da9d8a192e41b60fff0
kriverao98 <riveraocasiokoral29@gmail.com>
2024-09-25 13:36:29 -0400

    Add StudentClass and interface for Student in main.ts

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -19,4 +19,33 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
     return `${firstName.charAt(0)}. ${lastName}`;
 }
 
-console.log(printTeacher("John", "Doe"));
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+const student = new StudentClass("Jane", "Doe");
+console.log(student.workOnHomework());
+console.log(student.displayName());
