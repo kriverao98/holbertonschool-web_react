abb1a55b08a485763466d40abb46b18ec22dd1bc
kriverao98 <riveraocasiokoral29@gmail.com>
2024-09-26 01:09:39 -0400

    task 5 and 6

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,65 @@
+// DirectorInterface with 3 methods
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+// TeacherInterface with 3 methods
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// Director class implementing DirectorInterface
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
+
+// Teacher class implementing TeacherInterface
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+
+    workTeacherTasks(): string {
+        return "Getting to work";
+    }
+}
+
+// createEmployee function to return Director or Teacher instance
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+}
+// isDirector function - Type Predicate
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// executeWork function
+function executeWork(employee: Director | Teacher): void {
+    if (isDirector(employee)) {
+        console.log(employee.workDirectorTasks());
+    } else {
+        console.log(employee.workTeacherTasks());
+    }
+}
